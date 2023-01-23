88db3d613ed09acdffa8dc3974cc215b5d949f13
AiutoPcAmico <andrea@andreafelappi.it>
2023-01-23 12:58:19 +0100

    Fixed app name

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -44,7 +44,7 @@ function NavigationBar({ changedElement }) {
                             textDecoration: 'none',
                         }}
                     >
-                        LOGO
+                        WEATHERSTATION |
                     </Typography>
 
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
